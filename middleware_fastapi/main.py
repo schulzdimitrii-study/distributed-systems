1a39b49d7de7215201ab7ad58bad78bd533ae1de
Dimitri <dimitri.s.amado@gmail.com>
2026-04-14 23:18:54 -0300

    feat: Implement Middleware

--- middleware_fastapi/main.py
+++ middleware_fastapi/main.py
@@ -1,8 +1,10 @@
+from app.routes.middleware import add_process_time_header
 from app.routes.user import user_router
 from fastapi import FastAPI
 
 app = FastAPI()
 
+app.middleware("http")(add_process_time_header)
 app.include_router(user_router)
 
 @app.get("/")
