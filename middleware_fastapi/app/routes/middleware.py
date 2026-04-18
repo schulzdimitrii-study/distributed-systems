f0a31a4397c47da3ec12a8529667ffd1cda83132
Dimitri <dimitri.s.amado@gmail.com>
2026-04-18 14:22:46 -0300

    refactor: remove redundant http middleware decorator from process time header function

--- middleware_fastapi/app/routes/middleware.py
+++ middleware_fastapi/app/routes/middleware.py
@@ -2,7 +2,6 @@ import time
 
 from fastapi import Request
 
-@app.middleware("http")
 async def add_process_time_header(request: Request, call_next):
     start_time = time.perf_counter()
     response = await call_next(request)
